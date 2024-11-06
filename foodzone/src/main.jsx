8caa03421594775537c4a048a54a0d02dd80532e
varunjadav2002 <155519638+varunjadav2002@users.noreply.github.com>
2024-11-06 14:16:05 +0530

    initial commit

--- foodzone/src/main.jsx
+++ foodzone/src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import StoreContextProvider from './components/context/StoreContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App />
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
   </BrowserRouter>
 )
-
-
-
 
 
